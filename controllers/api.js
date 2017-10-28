139a6cce50c6c40290d3fc2e4f10efbb1934d0ae
Deyl <soulvisman@gmail.com>
2017-10-28 23:27:00 +0600

    added some functionality

--- controllers/api.js
+++ controllers/api.js
@@ -6,14 +6,25 @@ router.get('/', (req, res) => {
   res.end('This is api page');
 });
 router.get('/imagesearch/:query', (req, res) => {
-  const url = 'https://www.googleapis.com/customsearch/v1';
+  const api = 'https://www.googleapis.com/customsearch/v1';
   const key = 'AIzaSyDZ-NAjYtqtlAIY4k76C40DyQ6dUbCQpkg';
   const cx = '012819362395033083409:gwad5ihhulw';
   const query = req.params.query;
-  const fullUrl = `${url}?key=${key}&cx=${cx}&searchType=image&q=${query}`;
-  request.get(fullUrl, (err, response, body) => {
+  const off = req.query.offset || 1;
+  const url = `${api}?key=${key}&cx=${cx}&searchType=image&q=${query}&start=${off}`;
+  request.get(url, (err, response, body) => {
     if (!err && response.statusCode == 200){
-      res.json(JSON.parse(body).items);
+      let data = [];
+      body = JSON.parse(body).items;
+      body.forEach((val) => {
+        data.push({
+          url: val.link,
+          snippet: val.snippet,
+          thumbnail: val.image.thumbnailLink,
+          context: val.image.contextLink
+        });
+      });
+      res.json(data);
     }
   });
 });
